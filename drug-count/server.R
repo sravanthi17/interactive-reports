6ddfbf8f0b1c76be05510b4d037e8ac582d313fc
sravanthi17 <sravanns@thoughtworks.com>
2016-12-22 16:51:19 +0530

    Adding sample interactive bar chart (segragated by gender of the patient) for count of patients taking the choosen drugs

---
+++ drug-count/server.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(DBI)
+library(ggplot2)
+library(reshape2)
+
+shinyServer(function(input, output) {
+
+  output$drugPlot <-renderPlot({ 
+    conn1 <- dbConnect(
+      drv = RMySQL::MySQL(),
+      dbname = "openmrs",
+      
+      host = "10.0.1.103",
+      username = "openmrs-user",
+      password = "password")
+    on.exit(dbDisconnect(conn1), add = TRUE)
+    
+    dsamp1 <- dbGetQuery(conn1, paste0(
+      "select cn.name as name,
+      ifnull(sum(IF(p.gender = 'F', 1, 0)),0) AS female,
+      ifnull(sum(IF(p.gender = 'M', 1, 0)),0) AS male,
+      ifnull(sum(IF(p.gender = 'O', 1, 0)),0) AS other
+      from orders 
+      inner join concept_name cn on cn.concept_id = orders.concept_id 
+      inner join person p on p.person_id= orders.patient_id
+      where cn.name in(",paste0("'",paste(input$drugselected, collapse="', '"),"'"),") and orders.date_activated between ", paste0("'", as.character(input$fromdate), "'"), " and ", paste0("'", as.character(input$todate), "'")," group by orders.concept_id order by count(order_id) desc", ";"))
+    
+    DF1 <- melt(dsamp1, id.var="name")
+    ggplot(DF1, aes(x = name, y = value)) + 
+      geom_bar(aes(fill = variable), position = "dodge", stat="identity") + geom_text(aes(label=value), position = position_dodge(width = 1), vjust=-1) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + coord_flip()
+  }
+    )
+
+})
---
+++ drug-count/ui.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(DBI)
+library(ggplot2)
+
+
+conn <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "openmrs",
+  host = "10.0.1.103",
+  username = "openmrs-user",
+  password = "password")
+on.exit(dbDisconnect(conn), add = TRUE)
+dsamp <- dbGetQuery(conn, paste0("select cn.name as name, count(order_id) as count from orders inner join concept_name cn on cn.concept_id = orders.concept_id  group by orders.concept_id;"))
+drugnames <- dbGetQuery(conn, paste0("select cn.name as drugs, count(order_id) as count from orders inner join concept_name cn on cn.concept_id = orders.concept_id where cn.concept_name_type='FULLY_SPECIFIED' group by orders.concept_id having count(order_id) > 0;"))
+
+shinyUI(fluidPage(
+  titlePanel("Patient Count with drugs prescibed"),
+  sidebarPanel(
+    helpText("choose the drugs"),
+    selectInput("drugselected", "Drug Names:", 
+                choices=drugnames$drugs, multiple = TRUE),
+    hr(),
+    dateInput('fromdate',
+              label = 'Start Date input: yyyy-mm-dd',
+              value = Sys.Date()
+    ),
+    dateInput('todate',
+              label = 'End Date input: yyyy-mm-dd',
+              value = Sys.Date()
+    )
+  ),
+  mainPanel(
+    plotOutput("drugPlot",  click = "plot1_click") 
+  )
+))
